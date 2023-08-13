142ce7985badc2c4a7a399ddb10102e74c8c420b
SonmezCankurt <sonmezcankurt1262@gmail.com>
2023-08-13 22:37:11 +0300

    Add Dependency Inversion

---
+++ DependencyInversion/Program.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DependencyInversion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Switch sw1 = new Switch(new LightBulb());
+            sw1.Operate();
+            sw1.Operate();
+            sw1.Operate();
+
+            Switch sw2 = new Switch(new Door());
+            sw2.Operate();
+            sw2.Operate();
+            sw2.Operate();
+
+            Console.ReadLine();
+
+        }
+    }
+
+
+
+    // Good Design (Loose Coupling)
+
+    interface ISwitchable
+    {
+        bool IsActivated { get;}
+        void Activate();
+        void Deactivate();
+    }
+
+    class LightBulb : ISwitchable
+    {
+        private bool isActivated;
+        public bool IsActivated { get => isActivated; }
+
+        public void Activate()
+        {
+            isActivated = true;
+            Console.WriteLine("LightBulb is On");
+        }
+
+        public void Deactivate()
+        {
+            isActivated = false;
+            Console.WriteLine("LightBulb is Off");
+        }
+    }
+
+    class Door : ISwitchable
+    {
+        private bool isActivated;
+        public bool IsActivated
+        {
+            get
+            {
+                return isActivated;
+            }
+        }
+        public void Activate()
+        {
+            Console.WriteLine("Door is Open");
+        }
+
+        public void Deactivate()
+        {
+            Console.WriteLine("Door is Close");
+        }
+    }
+
+    class Switch
+    {
+        private ISwitchable device;
+
+        public Switch(ISwitchable device)
+        {
+            this.device = device;
+        }
+
+        public void Operate()
+        {
+            if (device.IsActivated)
+                device.Deactivate();
+            else
+                device.Activate();
+        }
+    }
+
+
+    // Bad Design (Tight Coupling)
+
+    /*
+    class LightBulb
+    {
+        public void TurnOn()
+        {
+            Console.WriteLine("LightBulb is ON");
+        }
+    }
+
+    class Switch
+    {
+        private LightBulb bulb;
+
+        public Switch()
+        {
+            bulb = new LightBulb();
+        }
+
+        public void Operate()
+        {
+            bulb.TurnOn();
+        }
+    }
+    */
+}
